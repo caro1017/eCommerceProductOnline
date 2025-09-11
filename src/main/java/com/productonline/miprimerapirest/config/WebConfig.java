3281f5716482bb0b96f0feadf6d65d9774c30f4d
Carolina Botero <uribebotero.carolina@gmail.com>
2025-09-11 00:50:46 -0500

    permisos de cors local y vercel

---
+++ src/main/java/com/productonline/miprimerapirest/config/WebConfig.java
@@ -0,0 +1,28 @@
+package com.productonline.miprimerapirest.config;
+
+import org.jetbrains.annotations.NotNull;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins(
+                                "http://localhost:3000",
+                                "https://product-online-ecommerce.vercel.app"
+                        )
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
